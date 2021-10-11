368b4d94a9a6a93eb93d8a159425eaa03e58b12a
Kratakey <Kratakey@gmail.com>
2021-10-11 13:08:45 +0300

    fix

--- src/test/java/setup/TestBase.java
+++ src/test/java/setup/TestBase.java
@@ -13,7 +13,6 @@ import static com.codeborne.selenide.Selenide.open;
 public class TestBase extends TestData{
     public String
             urlBase = "https://app.maxibooking.ru/",
-//            urlBase = "https://d8base.com/",
             urlLogin = urlBase + "/auth/login",
             urlLogOut = urlBase + "/auth/login?logout=",
             urlServicePublish = urlBase + "/service/publish",
