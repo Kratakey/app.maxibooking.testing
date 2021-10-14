b93ffc7d3c831df3edf6540e1866bbaaae144457
Kratakey <Kratakey@gmail.com>
2021-10-14 16:51:05 +0300

    changes

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -1,19 +1,15 @@
 package helpers;
 
 import com.codeborne.selenide.Selenide;
-import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-import static com.codeborne.selenide.Selenide.sleep;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
@@ -40,31 +36,11 @@ public class Attach {
         );
     }
 
-    //    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-//    public static String addVideo(String sessionId) {
-//        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-//                + getVideoUrl(sessionId)
-//                + "' type='video/mp4'></video></body></html>";
-//    }
-//
-    public static void addVideo(String sessionId) {
-        URL videoUrl = getVideoUrl(sessionId);
-        if (videoUrl != null) {
-            InputStream videoInputStream = null;
-            sleep(1000);
-
-            for (int i = 0; i < 10; i++) {
-                try {
-                    videoInputStream = videoUrl.openStream();
-                    break;
-                } catch (FileNotFoundException e) {
-                    sleep(1000);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
-        }
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String addVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + getVideoUrl(getSessionId())
+                + "' type='video/mp4'></video></body></html>";
     }
 
     public static URL getVideoUrl(String sessionId) {
@@ -78,4 +54,7 @@ public class Attach {
         return null;
     }
 
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
 }
--- src/test/java/setup/TestBase.java
+++ src/test/java/setup/TestBase.java
@@ -5,7 +5,9 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
@@ -18,15 +20,26 @@ public class TestBase extends TestData{
             urlServicePublish = urlBase + "/service/publish",
             urlOrders = urlBase + "/my-orders/inbox",
             urlUserRegistration = urlBase + "/auth/registration";
-
-    @BeforeEach
-    public void setupConfig() {
-//        Configuration.browser = "chrome";
+    @BeforeAll
+    public static void main(String[] args) {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.headless = true;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+//        Configuration.browser = "chrome";
 //        Configuration.startMaximized = true;
         Configuration.browserSize = "1920x1080";
         Configuration.timeout=8000;
+        Configuration.headless = true;
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+
+    @BeforeEach
+    public void setupConfig() {
         open(urlBase);
     }
 
@@ -35,6 +48,7 @@ public class TestBase extends TestData{
         Attach.screenshotAs("Screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
 }
